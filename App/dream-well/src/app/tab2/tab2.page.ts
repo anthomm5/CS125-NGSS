2334ebd0bb13d86ef8f960f83f41e04b06c7e5ba
anthomm5 <anthomm5@uci.edu>
2023-03-19 01:03:13 -0700

    Added sleep rec button functionality

--- App/dream-well/src/app/tab2/tab2.page.ts
+++ App/dream-well/src/app/tab2/tab2.page.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  result_str:String = "Some text";
   constructor() {}
+
+  get_sleep_recs() {
+    //document.getElementById("sleep_results").innerHTML
+
+    document.getElementById("sleep_results")!.innerHTML = 
+    "<ion-card color='light'> \
+      <ion-card-header> \
+        <ion-card-subtitle>Sleep Recommendation</ion-card-subtitle> \
+      </ion-card-header> \
+      <ion-card-content>8 Hours</ion-card-content> \
+    </ion-card>";
+  }
 
 }
